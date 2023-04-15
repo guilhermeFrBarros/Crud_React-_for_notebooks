ae911d43f807e0b818c9ec95fb3d473f469f4273
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-04-15 12:13:11 -0300

    refactored component Nav

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react';
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        {/*Refatorar */}
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home" ></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-laptop" aria-hidden="true"></i> Notebooks
+            </a>
+        </nav>
     </aside>
