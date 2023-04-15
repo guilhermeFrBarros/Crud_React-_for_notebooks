b21373fc2a0627bcf7ae2567dcc2c0a301a1902a
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-04-15 12:21:26 -0300

    Home component created and added in App.jsx

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import Main from "../template/MainComp";
+
+
+import React from "react";
+
+export default props =>
+<Main icon="home" title="inicio"                    /* o icon home veio do font-awonso*/
+    subtitle="Crud de Notebooks">
+    <div className="display-4">Bem Vindo</div>
+    <hr />
+    <p className="mb-0">Um Sistema de Crud Notebooks,
+        Descktop e mini pcs</p>
+</Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,14 +5,13 @@ import React from 'react';
 
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
-import Main from '../components/template/MainComTe';
+import Home from '../components/home/Home';
 import Footer from '../components/template/Footer';
 
 export default props =>
     <div className="app">
         <Logo/>
         <Nav/>
-        <Main icon="home" title="inicio"                    /* o icon home veio do font-awonso*/
-            subtitle="Crud de Notebooks"/>
+        <Home/>
         <Footer/>
     </div>
