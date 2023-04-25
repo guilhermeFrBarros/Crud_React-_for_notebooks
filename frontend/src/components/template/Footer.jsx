305af75f2489232f0fe7e85845f1f15883d72286
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-04-25 20:31:28 -0300

    Modified Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 export default props =>
     <footer className="footer">
         <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-            <strong> Guilherme <span className="text-danger">Barros</span></strong>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+            <strong> André <span className="text-danger">Luiz</span> Luiz
+             e  Guilherme <span className="text-danger">Barros</span></strong>
         </span>
     </footer>
